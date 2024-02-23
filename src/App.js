b49851ad5863f22db3ed84ef8244d0a7d2436fd8
jayeshpatel882006 <jayu07498@gmail.com>
2024-02-23 17:52:58 +0530

    Project Uploaded

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar from "./Components/Navbar";
+import QR_Comp from "./Components/QR_Comp";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="Main">
+        <QR_Comp />
+      </div>
+    </div>
+  );
+}
+
+export default App;
